84652ccd433553a6e515ab19137a7c27d035cde4
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2017-04-29 17:10:35 -0400

    Added error checking

--- app.js
+++ app.js
@@ -20,7 +20,16 @@ request({
 	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${input}`,
 	json: true
 }, (error, response, body) => {
-	console.log(`Address: ${body.results[0].formatted_address}`);
-	console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
-	console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+	if (error) {
+		console.log('Unable able to connect to Google servers');
+	} else if (body.status === 'ZERO_RESULTS') {
+		console.log('Unable to find that address');
+	} else if (body.status === 'OK') {
+		console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+		console.log(`Longitude: ${body.results[0].geometry.location.lng}`)	
+	} else {
+		console.log('Invalid input - no data found!');
+	}
+	
 })
