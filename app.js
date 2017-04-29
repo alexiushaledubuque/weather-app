b732cc04c504e79355f68ee82d4288161d070040
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2017-04-29 16:51:17 -0400

    Dynamically fetching address with api

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Address to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
+
+var input = encodeURIComponent(argv.a);
 
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${input}`,
 	json: true
 }, (error, response, body) => {
 	console.log(`Address: ${body.results[0].formatted_address}`);
-	console.log(`Lat: ${body.results[0].geometry.location.lat}`)
-	console.log(`Lng: ${body.results[0].geometry.location.lng}`)
+	console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+	console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
 })
