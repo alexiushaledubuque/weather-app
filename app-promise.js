e2a2d06c19ff687183bca73b18ee8f73a9070218
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2017-04-30 13:25:08 -0400

    More weather.app refactoring with axios

--- app-promise.js
+++ app-promise.js
@@ -18,10 +18,26 @@ var encodedAddress = encodeURIComponent(argv.address);
 var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
 axios.get(geocodeUrl).then((response) => {
-	console.log(response.data);
+	if (response.data.status === 'ZERO_RESULTS') {
+		throw new Error('Unable to find that address'); // immediately stops running & jumps to .catch
+	}
+
+	var lat = response.data.results[0].geometry.location.lat;
+	var lng = response.data.results[0].geometry.location.lng;
+
+	var weatherUrl = `https://api.darksky.net/forecast/7a111862a24c6ef25a2deff1b62cff88/${lat},${lng}
+		`
+	console.log(response.data.results[0].formatted_address);
+	return axios.get(weatherUrl);
+}).then((response) => {
+	var temperature = response.data.currently.temperature;
+	var apparentTemperature = response.data.currently.apparentTemperature;
+	console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
 }).catch((e) => {
 	if (e.code === 'ECONNREFUSED') {
 		console.log('Unable to connect to API servers.');	
+	} else {
+		console.log(e.message);
 	}
 });
 
